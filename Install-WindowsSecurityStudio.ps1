095554d7652a2ec528f492ddb367ecd5ccf6d9cf
offsechq <101721830+OFFSECHQ@users.noreply.github.com>
2026-01-30 16:28:01 +0530

    streamline install: unified Install.cmd, cert as release asset, auto-trust in updates

--- Install-WindowsSecurityStudio.ps1
+++
@@ -1,9 +0,0 @@
-function P {
-    Write-Warning -Message "The module you're trying to install has been deprecated; Please install the new System Security Studio app from: https://github.com/OFFSECHQ/windows-security-studio/releases"
-}
-function AppControl {
-    [CmdletBinding()]
-    param ([Parameter(Mandatory = $false)][string]$MSIXBundlePath, [Parameter(Mandatory = $False)][string]$SignTool)
-    Write-Warning -Message 'Please install the App Control Studio from: https://github.com/OFFSECHQ/windows-security-studio/releases'
-    Write-Warning -Message 'Both the App Control Studio and the System Security Studio apps are capable of installing any app package with any certificate.'
-}
